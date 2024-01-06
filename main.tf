3b554dd8624d8cf341d53c3e8bf9d410a5ee64bb
Frank DiRocco <160608+fdr2@users.noreply.github.com>
2024-01-06 12:30:52 -0500

    ensure maps are read instead of direct values

--- main.tf
+++ main.tf
@@ -29,8 +29,9 @@ resource "vault_pki_secret_backend_root_cert" "root" {
 }
 
 resource "vault_pki_secret_backend_issuer" "root" {
-  backend     = vault_pki_secret_backend_root_cert.root.backend
-  issuer_ref  = vault_pki_secret_backend_root_cert.root.issuer_id
+  for_each    = var.pki_map
+  backend     = vault_pki_secret_backend_root_cert.root[each.key].backend
+  issuer_ref  = vault_pki_secret_backend_root_cert.root[each.key].issuer_id
   issuer_name = "default"
 }
 
