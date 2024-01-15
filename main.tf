fb78c5368b22992eb31e5e12ed7cb596f48e4e44
Frank DiRocco <160608+fdr2@users.noreply.github.com>
2024-01-15 14:22:55 -0500

    remove issuer ref from intermediate role

--- main.tf
+++ main.tf
@@ -109,5 +109,4 @@ resource "vault_pki_secret_backend_role" "intermediate" {
   allow_ip_sans      = try(each.value["allow_ip_sans"], null)
   allowed_uri_sans   = try(each.value["allowed_uri_sans"], null)
   allowed_other_sans = try(each.value["allowed_other_sans"], null)
-  issuer_ref         = vault_pki_secret_backend_issuer.intermediate[each.key].issuer_ref
 }
